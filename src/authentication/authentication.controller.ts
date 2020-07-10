517e5f61730832ac1c6c14dc2fb89e5fe8d91869
DanLide <danil.lidzer@gmail.com>
2020-07-10 05:06:45 +0000

    implemented basic authentication with Passport

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import RegisterDto from './dto/register.dto';
+import { LocalAuthenticationGuard } from './guards/localAuthentication.guard';
+import RequestWithUser from './interfaces/requestWithUser.interface';
 
 @Controller('authentication')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(
+    private readonly authenticationService: AuthenticationService,
+  ) {}
+
+  @Post('register')
+  async register(@Body() registrationData: RegisterDto) {
+    return this.authenticationService.register(registrationData);
+  }
+
+  @HttpCode(200)
+  @UseGuards(LocalAuthenticationGuard)
+  @Post('log-in')
+  async logIn(@Req() request: RequestWithUser) {
+    const { user } = request;
+    user.password = undefined;
+    return user;
+  }
+}
--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  controllers: [AuthenticationController],
+  imports: [UsersModule, PassportModule],
+  controllers: [AuthenticationController, LocalStrategy],
   providers: [AuthenticationService]
 })
 export class AuthenticationModule {}
